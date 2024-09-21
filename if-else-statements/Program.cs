e917febc930d038588e817fdc5cd67f320db01f9
Gmentesh2 <g.menteshashvili.sg18@gipa.ge>
2024-09-21 17:08:43 +0400

    if/else

---
+++ if-else-statements/Program.cs
@@ -0,0 +1,189 @@
+﻿namespace if_else_statements
+    {
+    internal class Program
+        {
+        static void Main( string[] args )
+            {
+            //1
+            //Console.WriteLine("Enter first number: ");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Enter second number: ");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //double sum = number1 + number2;
+            //Console.WriteLine($"The sum of the numbers is: {sum}");
+
+            //2
+
+            //Console.WriteLine("Enter number: ");
+            //int number1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter second number: ");
+            //int number2 = int.Parse(Console.ReadLine());
+
+            //int sum = number1 * number2;
+
+            //if (sum > 10)
+            //    {
+            //    Console.WriteLine($"Result > 10 is true");
+            //    }
+            //else
+            //    {
+            //    Console.WriteLine($"Result > 10 is false");
+            //    }
+
+            //3
+
+            //Console.WriteLine("Enter the number: ");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter second number: ");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //double sum = number1 + number2;
+
+            //bool isEven = false;
+
+
+
+            //if (sum % 2 == 0)
+            //    {
+            //    isEven = true;
+            //    Console.WriteLine($"The sum of the numbers is: {sum} and its even");
+            //    }
+            //else
+            //    {
+            //    isEven = false;
+            //    Console.WriteLine($"The sum of the numbers is: {sum} and its odd");
+            //    }
+
+            //4
+
+            //Console.WriteLine("Enter 2 numbers and choose your desired operation: [ + , - , * , /] ");
+
+            //int number1 = int.Parse(Console.ReadLine());
+            //int number2 = int.Parse(Console.ReadLine());
+
+            //string operation = Console.ReadLine();
+
+            //if (operation == "+")
+            //    {
+            //    int sum = number1 + number2;
+            //    Console.WriteLine($"You choose + operator and the sum of the numbers is {sum}");
+            //    }
+            //else if (operation == "-")
+            //    {
+            //    int substraction = number1 - number2;
+            //    Console.WriteLine($"You choose - operator and the sum of the numbers is {substraction}");
+            //    }
+            //else if (operation == "*")
+            //    {
+            //    int multiply = number1 * number2;
+            //    Console.WriteLine($"You choose * operator and the sum of the numbers is {multiply}");
+            //    }
+            //else if (operation == "/")
+            //    {
+            //    int divide = number1 / number2;
+            //    Console.WriteLine($"You choose / operator and the sum of the numbers is {divide}");
+            //    }
+            //else
+            //    {
+            //    Console.WriteLine("Enter valid numbers or operations");
+            //    }
+
+
+            //switch (operation)
+            //    {
+            //    case "+":
+            //        int sum = number1 + number2;
+            //        Console.WriteLine($"You choose + operator and the sum of the numbers is {sum}");
+            //        break;
+            //    case "-":
+            //        int substraction = number1 - number2;
+            //        Console.WriteLine($"You choose - operator and the sum of the numbers is {substraction}");
+            //        break;
+            //    case "*":
+            //        int multiply = number1 * number2;
+            //        Console.WriteLine($"You choose * operator and the sum of the numbers is {multiply}");
+            //        break;
+            //    case "/":
+            //        int divide = number1 / number2;
+            //        Console.WriteLine($"You choose / operator and the sum of the numbers is {divide}");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Enter valid numbers or operators");
+            //        break;
+            //    }
+
+            //5
+
+            //Console.WriteLine("Enter number and percent: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //int percent = int.Parse(Console.ReadLine());
+
+            //if (percent > 0)
+            //    {
+            //    int result = number * percent / 100;
+            //    Console.WriteLine($"{number} {percent} percent is {result}");
+            //    }
+            //else
+            //    {
+            //    Console.WriteLine("Error");
+            //    }
+
+            //6
+
+            //Console.WriteLine("Enter 6 digit number: ");
+
+            //string number = Console.ReadLine();
+
+            //if (number.ToString().Length == 6)
+            //    {
+
+            //    int pos1 = int.Parse(Console.ReadLine()) - 1;
+            //    int pos2 = int.Parse(Console.ReadLine()) - 1;
+
+            //    char[] digits = number.ToCharArray();
+            //    (digits[pos1], digits[pos2]) = (digits[pos2], digits[pos1]);
+
+            //    Console.WriteLine($"Swapped number: {new string(digits)}");
+
+            //    }
+            //else
+            //    {
+            //    Console.WriteLine("Error");
+            //    }
+
+
+            //7
+
+            //Console.WriteLine("Choose conversion: ");
+            //Console.WriteLine("1 - Fahrenheit to Celsius");
+            //Console.WriteLine("2 - Celsius to Fahrenheit");
+
+            //int choice = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the temperature:");
+            //double temperature = double.Parse(Console.ReadLine());
+
+
+            //if (choice == 1)
+            //    {
+
+            //    double celsius = (temperature - 32) * 5 / 9;
+            //    Console.WriteLine($"The temperature in Celsius is: {celsius:F2}");
+            //    }
+            //else if (choice == 2)
+            //    {
+
+            //    double fahrenheit = (temperature * 9 / 5) + 32;
+            //    Console.WriteLine($"The temperature in Fahrenheit is: {fahrenheit:F2}");
+            //    }
+            //else
+            //    {
+            //    Console.WriteLine("Invalid choice. Please select 1 or 2.");
+            //    }
+            }
+        }
+    }
